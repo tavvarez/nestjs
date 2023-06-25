df8e1bd5033b71fcb58c064a5494631905d46f56
tavvarez <gabrieldaraujotavares@gmail.com>
2023-06-25 15:14:37 +0000

    prisma service

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { PrismaService } from './prisma/prisma.service';
 
-@Module({})
+@Module({
+  providers: [PrismaService]
+})
 export class PrismaModule {}
